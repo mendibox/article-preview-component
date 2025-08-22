a640c9522a8268ed6b6ec240626dc8f5abe69798
Juan Luis Mendiola <mendibox@gmail.com>
2025-08-22 13:32:09 -0600

    Sharing popup is enabled

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+const authorIcon =  document.querySelector('.article__author-icon');
+const authorIconActive = document.querySelector('.article__author-icon_active');
 const articleShare = document.querySelector('.article__share');
-const articleAuthorIcon = document.querySelectorAll('.article__author-icon');
 
-articleAuthorIcon.forEach.addEventListener('click', () => {
-    articleShare.toggleAttribute('article__share_active');
+authorIcon.addEventListener('click', () =>{
+    articleShare.classList.add('article__share_active');
 });
+
+authorIconActive.addEventListener('click', () => {
+    articleShare.classList.remove('article__share_active');
+})
