8f87506ed79443cdb3115a7b736360a175a4f506
Juan Luis Mendiola <mendibox@gmail.com>
2026-04-14 12:34:48 -0600

    share icon is still imperfect

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+const articleAuthor = document.querySelector('.article__author');
 const authorIcon =  document.querySelector('.article__author-icon');
-const authorIconActive = document.querySelector('.article__author-icon_active');
+const authorIconActive = document.querySelector('.article__author-icon-active');
 const articleShare = document.querySelector('.article__share');
+const articleShareActive = document.querySelector('.article__share-active')
 
 authorIcon.addEventListener('click', () =>{
-    articleShare.classList.toggle('article__share_active');
+    articleAuthor.classList.add('article__author-active');
+    articleShare.classList.add('article__share-active');
 });
 
 authorIconActive.addEventListener('click', () => {
-    articleShare.classList.remove('article__share_active');
-})
+    articleShare.classList.remove('article__share-active');
+    articleAuthor.classList.remove('article__author-active');
+});
