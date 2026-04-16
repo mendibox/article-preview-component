77173645903ce6bcd29f2e3175709b8ce3764f39
Juan Luis Mendiola <mendibox@gmail.com>
2026-04-16 10:46:36 -0600

    finished mobile design

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-const articleAuthor = document.querySelector('.article__author');
 const authorIcon =  document.querySelector('.article__author-icon');
 const authorIconActive = document.querySelector('.article__author-icon-active');
 const articleShare = document.querySelector('.article__share');
 const articleShareActive = document.querySelector('.article__share-active')
 
 authorIcon.addEventListener('click', () =>{
-    articleAuthor.classList.add('article__author-active');
     articleShare.classList.add('article__share-active');
 });
 
 authorIconActive.addEventListener('click', () => {
     articleShare.classList.remove('article__share-active');
-    articleAuthor.classList.remove('article__author-active');
 });
