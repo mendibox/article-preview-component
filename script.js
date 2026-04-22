95ff9a382410eaa114b3b76a764a3cc032aa9c54
Juan Luis Mendiola <mendibox@gmail.com>
2026-04-22 13:31:33 -0600

    challenge finished

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const authorIcon =  document.querySelector('.article__author-icon');
 const authorIconActive = document.querySelector('.article__author-icon-active');
 const articleShare = document.querySelector('.article__share');
 const articleShareActive = document.querySelector('.article__share-active')
+const shape = document.querySelector('.shape');
 
 authorIcon.addEventListener('click', () =>{
     articleShare.classList.add('article__share-active');
+    shape.classList.add('shape-active');
 });
 
 authorIconActive.addEventListener('click', () => {
     articleShare.classList.remove('article__share-active');
+    shape.classList.remove('shape-active');
 });
